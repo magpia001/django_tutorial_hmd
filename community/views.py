5a53c5becde5f72181bd15e27c386c7f6f6deeb7
kyongha <magpia001@gmail.com>
2023-03-27 17:33:31 +0900

    community앱생성, Article모델정의, Form 글작성, DB저장 기능구현

--- community/views.py
+++ community/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
+from community.forms import Form
 
 # Create your views here.
+
+# http://127.0.0.1:8000/write/
 def write(request):
+    # 입력한 form 데이터를 받아서 DB에 저장
+    if request.method == 'POST':
+        form = Form(request.POST)
+        # form 데이터의 유효성 검사
+        if form.is_valid():
+            # form 데이터 저장
+            form.save()
+    form = Form()
+    # else:
+      # 빈 form으로 응답하기
+      # form = Form()
     
-    # 방법1
-    name = "joy"
-    title = "오늘 저녁에 운동하기"
-    content = "친구랑 함께 한강변을 걷기로함"
-
-    # return render(request, 'write.html', {
-    #     "name": name, 
-    #     "title": title, 
-    #     "content":content})
-
-    # 방법2
-    context =  {
-      "name" : name, 
-      "title" : title, 
-      "content" : content
-    }
-    return render(request, 'write.html', context)
-
+    return render(request, 'write.html', {"form": form})
