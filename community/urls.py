9b6064c91ce4260aa52efe9fc027b2516b67a870
kyongha <magpia001@gmail.com>
2023-03-31 16:30:11 +0900

    navbar에 url 템플릿 태그 적용

--- community/urls.py
+++ community/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 # from community.views import write, list
 from community import views
-
+ 
+# app_name="community"    
 urlpatterns = [
     # write/ 요청이 들어오면, views.write()함수 실행
     path('write/', views.write, name="wirte"),
-    # list/
-    path('list/', views.list, name="list"),
+    # community/list/
+    path('lists/', views.list, name="list"),
     # view_detail/4
     path('view_detail/<int:num>/', views.viewDetail, name="view_detail"),
 ]
