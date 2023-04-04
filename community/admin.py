deb0af1ec7cbbc03d7e5ff0cda816a3c783956fd
kyongha <magpia001@gmail.com>
2023-04-04 14:47:58 +0900

    admin site UI 변경 적용

--- community/admin.py
+++ community/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+  # fields = ['name', 'title', 'contents', 'url', 'email']
+  fieldsets = [
+    ('제목', {'fields': ['title']}),
+    ('내용', {'fields': ['contents']}),
+    ('작성자 정보', {'fields': ['name', 'url', 'email']}),
+    ('작성자ID', {'fields': ['owner'], 'classes':['collapse']})
+    # ('', {'fields': ['']}),
+  ]
+
+  list_display = ('pk', 'title', 'url', 'cdate')
+  list_filter = ['cdate']
+  search_fields = ['title', 'contents']
+
+# 전달인자 순서 중요
+# admin.site.register(데이터모델클래스, 어드민클래스)
+admin.site.register(Article, ArticleAdmin)
--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import CountryData
 
 # Register your models here.
-admin.site.register(CountryData)
+
+# admin site dashbord list ui 바꾸기
+# UI 변경 클래스 정의
+## admin.ModelAdmin 상속 받기
+# admin.site.register(모델클래스, UI재정의클래스)
+
+class DashboardAdmin(admin.ModelAdmin):
+    list_display = ('country', 'population')
+    
+admin.site.register(CountryData, DashboardAdmin)
